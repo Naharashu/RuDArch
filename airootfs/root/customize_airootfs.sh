35dc90063861bc71d123f3bea855777e8e9a04c3
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-08-23 23:01:33 +0300

    dev3

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+chmod 755 /usr/local/bin/choose-mirror
+chmod 755 /usr/local/bin/Installation_guide  
+chmod 755 /usr/local/bin/livecd-sound
+chmod 755 /usr/local/bin/rudarch-install
+chmod 755 /root/.automated_script.sh
--- profiledef.sh
+++ profiledef.sh
@@ -12,7 +12,7 @@ bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-b' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 declare -A file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -22,4 +22,6 @@ declare -A file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/rudarch-install"]="0:0:755"
+  ["/root/customize_airootfs.sh"]="0:0:755"
 )
